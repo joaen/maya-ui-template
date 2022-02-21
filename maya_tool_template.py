3444c1dab015a0ff1a4a44cacfa3a08803b78291
Joaen <joar.engberg@gmail.com>
2022-02-21 11:32:53 +0100

    Update maya_tool_template.py

--- maya_tool_template.py
+++ maya_tool_template.py
@@ -2,8 +2,7 @@ import sys
 import maya.cmds as cmds
 import maya.OpenMayaUI as omui
 from shiboken2 import wrapInstance
-from PySide2 import QtCore
-from PySide2 import QtWidgets
+from PySide2 import QtCore, QtGui, QtWidgets
 
 
 def maya_main_window():
@@ -29,19 +28,38 @@ class TemplateToolWindow(QtWidgets.QDialog):
             self.setWindowFlags(QtCore.Qt.Tool)
  
     def create_ui_widgets(self):
+        self.template_label = QtWidgets.QLabel("TEMPLATE_TEXT_LABEL")
         self.template_button = QtWidgets.QPushButton("TEMPLATE BUTTON")
-        
-    def create_ui_layout(self):
+        self.template_checkbox = QtWidgets.QCheckBox("TEMPLATE_CHECKBOX")
+        self.template_combobox = QtWidgets.QComboBox()
+        self.template_combobox.addItem("TEMPLATE_COMBOBOX_ITEM")
+        self.template_textfield = QtWidgets.QLineEdit()
+        self.template_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.template_icon_button = QtWidgets.QPushButton()
+        self.template_icon_button.setIcon(QtGui.QIcon(":fileOpen.png"))
 
-        lower_row_buttons = QtWidgets.QHBoxLayout()
-        lower_row_buttons.addWidget(self.template_button)
- 
+    def create_ui_layout(self):
+        horizontal_layout = QtWidgets.QHBoxLayout()
+        horizontal_layout.addWidget(self.template_textfield)
+        horizontal_layout.addWidget(self.template_icon_button)
+        horizontal_layout.addWidget(self.template_button)
+
+        vertical_layout = QtWidgets.QVBoxLayout()
+        vertical_layout.addWidget(self.template_label)
+        vertical_layout.addWidget(self.template_checkbox)
+        vertical_layout.addWidget(self.template_combobox)
+        vertical_layout.addWidget(self.template_slider)
+        vertical_layout.addStretch()
+
         main_layout = QtWidgets.QVBoxLayout(self)
         main_layout.setContentsMargins(10, 10, 10, 10)
-        main_layout.addLayout(lower_row_buttons)
+        main_layout.addLayout(horizontal_layout)
+        main_layout.addLayout(vertical_layout)
  
     def create_ui_connections(self):
         self.template_button.clicked.connect(self.template_command)
+        self.template_icon_button.clicked.connect(self.template_command)
+        self.template_slider.valueChanged.connect(self.template_command)
 
     def template_command(self):
         print("TEMPLATE!")
